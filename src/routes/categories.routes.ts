b9744a25d8ffae57d42764094e8bdc66c20957a5
Guilherme Linhares Rocha <guilhermelinharesr@gmail.com>
2021-12-08 09:57:21 -0300

    refatorando a listagem de categoria

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { CategoriesRepository } from '../modules/cars/repositories/CategoriesRepository';
 import { createCategoryController } from '../useCases/createCategory';
+import { listCategoriesController } from '../useCases/listCategories';
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
@@ -10,9 +11,7 @@ categoriesRoutes.post("/", (request, response) => {
 })
 
 categoriesRoutes.get("/", (request, response) => {
-    const all = categoriesRepository.list();
-    
-    return response.status(201).json(all);
+    return listCategoriesController.handle(request, response);
 })
 
 export { categoriesRoutes };
