9e63047447a65699add330a81b15d251ecfb084a
Guilherme Linhares Rocha <guilhermelinharesr@gmail.com>
2021-12-08 11:01:44 -0300

    criando upload de arquivos

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,16 +1,26 @@
-import { Router } from 'express';
-import { CategoriesRepository } from '../modules/cars/repositories/implementations/CategoriesRepository';
+import { request, Router } from 'express';
+import multer from 'multer'
+
 import { createCategoryController } from '../useCases/createCategory';
 import { listCategoriesController } from '../useCases/listCategories';
 
 const categoriesRoutes = Router();
 
+const upload = multer({
+    dest: "./tmp",
+})
+
 categoriesRoutes.post("/", (request, response) => {
    return createCategoryController.handle(request, response);
-})
+});
 
 categoriesRoutes.get("/", (request, response) => {
     return listCategoriesController.handle(request, response);
-})
+});
+
+categoriesRoutes.post("/import", upload.single("file"), (request, response) => {
+    const { file } = request;
+    return response.send();
+});
 
 export { categoriesRoutes };
