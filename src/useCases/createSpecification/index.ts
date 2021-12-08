ac5bbddb8b223fa7ede6dee130dd049129c3cf36
Guilherme Linhares Rocha <guilhermelinharesr@gmail.com>
2021-12-08 10:29:17 -0300

    criando use case especificacao

---
+++ src/useCases/createSpecification/index.ts
@@ -0,0 +1,12 @@
+import { SpecificationsRepository } from "../../modules/cars/repositories/implementations/SpecificationsRepository";
+import { CreateSpecificationController } from "./createSpecificationController";
+import { CreateSpecificationUseCase } from "./CreateSpecificationUseCase";
+
+const specificationsRepository = new SpecificationsRepository();
+
+const createSpecificationUseCase = new CreateSpecificationUseCase(specificationsRepository);
+
+const createSpecificationController = new CreateSpecificationController(createSpecificationUseCase);
+
+export { createSpecificationController }
+
